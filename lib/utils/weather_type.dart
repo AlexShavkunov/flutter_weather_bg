7a4701367dd4b8a71cd6ba3c771be0e06cd5f4a0
Alex Shavkunov <AleeexShavkunov@gmail.com>
2020-12-28 21:30:15 +0500

    Update weather_type.dart

--- lib/utils/weather_type.dart
+++ lib/utils/weather_type.dart
@@ -54,39 +54,7 @@ class WeatherUtil {
 
   // 根据天气类型获取背景的颜色值
   static List<Color> getColor(WeatherType weatherType) {
-    switch (weatherType) {
-      case WeatherType.sunny:
-        return [Color(0xFF0071D1), Color(0xFF6DA6E4)];
-      case WeatherType.sunnyNight:
-        return [Color(0xFF061E74), Color(0xFF275E9A)];
-      case WeatherType.cloudy:
-        return [Color(0xFF5C82C1), Color(0xFF95B1DB)];
-      case WeatherType.cloudyNight:
-        return [Color(0xFF2C3A60), Color(0xFF4B6685)];
-      case WeatherType.overcast:
-        return [Color(0xFF8FA3C0), Color(0xFF8C9FB1)];
-      case WeatherType.lightRainy:
-        return [Color(0xFF556782), Color(0xFF7c8b99)];
-      case WeatherType.middleRainy:
-        return [Color(0xFF3A4B65), Color(0xFF495764)];
-      case WeatherType.heavyRainy:
-      case WeatherType.thunder:
-        return [Color(0xFF3B434E), Color(0xFF565D66)];
-      case WeatherType.hazy:
-        return [Color(0xFF989898), Color(0xFF4B4B4B)];
-      case WeatherType.foggy:
-        return [Color(0xFFA6B3C2), Color(0xFF737F88)];
-      case WeatherType.lightSnow:
-        return [Color(0xFF283584), Color(0xFF376AB2)];
-      case WeatherType.middleSnow:
-        return [Color(0xFF8595AD), Color(0xFF95A4BF)];
-      case WeatherType.heavySnow:
-        return [Color(0xFF98A2BC), Color(0xFFA7ADBF)];
-      case WeatherType.dusty:
-        return [Color(0xFFB99D79), Color(0xFF6C5635)];
-      default:
-        return [Color(0xFF0071D1), Color(0xFF6DA6E4)];
-    }
+    return [Colors.transparent, Colors.transparent];
   }
 
   // 根据天气类型获取天气的描述信息
